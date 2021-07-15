f49a7451535b3d2459db8474c848105218faac39
Mateo <jmartinez6208@uta.edu.ec>
2021-07-15 15:54:07 -0500

    15 Julio falla

--- hello.js
+++ hello.js
@@ -6,7 +6,7 @@ http.createServer(function (req, res) {
 
   res.writeHead(200, {'Content-Type': 'text/plain'});
 
-  res.end('Hello World\n'); // missing semi-colon will fail the build
+  res.end('Hello World\n') // missing semi-colon will fail the build
 
 }).listen(1337, '127.0.0.1');
 
